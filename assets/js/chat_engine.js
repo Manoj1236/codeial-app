70644a72a32c6e808afdfff40262f6f045bcdc5a
Manoj Desai <desaimanoj052@gmail.com>
2023-02-26 15:56:05 +0530

    socket error corrected

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -3,7 +3,7 @@ class ChatEngine{
         this.chatBox = $(`#${chatBoxId}`);
         this.userEmail = userEmail;
 
-        this.socket = io.connect('http://localhost:5000');
+        this.socket = io.connect('http://http://43.206.149.185:5000');
 
         if (this.userEmail){
             this.connectionHandler();
